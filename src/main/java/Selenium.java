9220be8c2c3744b3c3f6f9d109ed3a6351bd03de
Denis Shepelev <den.shep@yahoo.com>
2015-02-04 19:34:29 -0800

    Initial commit

---
+++ src/main/java/Selenium.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Created by ds on 2/4/15.
+ */
+public class Selenium {
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver(); // Version 1.1 :: Firefox
+
+        String test_case_id = "TC-001.01";
+        String url = "http://www.dpreview.com";
+        String title_expected = "Digital Photography Review: Digital Photography Review";
+
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        String title_actual = driver.getTitle();
+
+        if (title_expected.equals(title_actual)) {
+            System.out.println("Test Case ID: \t\t" + test_case_id);
+            System.out.println("URL: \t\t" + url);
+            System.out.println("Title Expected: \t\t" + title_expected);
+            System.out.println("Test Case Result: \t\t" + "PASSED");
+        } else {
+            System.out.println("Test Case ID: \t\t" + test_case_id);
+            System.out.println("URL: \t\t" + url);
+            System.out.println("Title Expected: \t\t" + title_expected);
+            System.out.println("Test Case Result: \t\t" + "FAILED");
+        }
+        driver.quit();
+    }
+}
